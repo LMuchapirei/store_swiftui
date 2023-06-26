ef5cea37a1ce086cbccec0fb22f9aa553950da2c
LMuchapirei <h170407h@hit.ac.za>
2023-06-26 18:01:59 +0200

    Initial Commit

---
+++ store_swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  store_swiftui
+//
+//  Created by Linval Muchapirei on 26/6/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ store_swiftui/store_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  store_swiftuiApp.swift
+//  store_swiftui
+//
+//  Created by Linval Muchapirei on 26/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct store_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
