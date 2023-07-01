db0aa53772f0d19b13eb6cc8bd64e4dcb5364890
LMuchapirei <h170407h@hit.ac.za>
2023-07-01 23:41:51 +0200

    Cart button with num of items indicator

---
+++ store_swiftui/Components/CartButton.swift
@@ -0,0 +1,34 @@
+//
+//  CartButton.swift
+//  store_swiftui
+//
+//  Created by Linval Muchapirei on 1/7/2023.
+//
+
+import SwiftUI
+
+struct CartButton: View {
+    var numberOfProducts: Int
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+            Image(systemName: "bag.fill")
+                .foregroundColor(.black)
+                .padding(5)
+            if numberOfProducts > 0 {
+                Text("\(numberOfProducts)")
+                    .font(.caption2)
+                    .foregroundColor(.white)
+                    .frame(width: 15,height: 15)
+                    .background(.green)
+                    .cornerRadius(50)
+                
+            }
+        }
+    }
+}
+
+struct CartButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CartButton(numberOfProducts: 4)
+    }
+}
