bbbe7540be0f3bbb2d944086946cc18159fa501a
LMuchapirei <h170407h@hit.ac.za>
2023-07-01 23:41:13 +0200

    Cart manager to handle products in cart

---
+++ store_swiftui/CartManger.swift
@@ -0,0 +1,26 @@
+//
+//  CartManger.swift
+//  store_swiftui
+//
+//  Created by Linval Muchapirei on 1/7/2023.
+//
+
+import Foundation
+
+
+class CartManager: ObservableObject {
+    @Published private (set) var products: [Product] = []
+    @Published private (set) var total:Int = 0
+    
+    func addToCart(product: Product){
+        products.append(product)
+        total += product.price
+    }
+    
+    func removeFromCart(product: Product){
+        products = products.filter{
+            $0.id != product.id
+        }
+        total -= product.price
+    }
+}
